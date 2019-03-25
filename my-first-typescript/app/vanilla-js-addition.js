863f5de8b06664cf064fd4c611ad4f58ce952459
Robbie <robbiekruszynski@gmail.com>
2019-03-24 20:48:24 -0700

    add ts work

---
+++ my-first-typescript/app/vanilla-js-addition.js
@@ -0,0 +1,4 @@
+var firstNumber: number = prompt('Please enter a number.');
+var secondNumber: number = prompt('Enter another number.');
+var sum: number = firstNumber + secondNumber;
+alert("The sum of your two numbers is: " + sum);
